9138d763d94798b5f5d7e2707294dc71b71795d8
Wu Feiyang <feiyangw.21@intl.zju.edu.cn>
2024-07-03 16:23:29 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,94 @@
+"""
+Created on Wed July 3 12:38:21 2024
+
+@author: Feiyang Wu
+
+Python 3.5.2
+"""
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from snn import *
+import time
+import os
+
+names = 'spiking_model'
+data_path =  './raw/' 
+batch_size = 100
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+train_dataset = torchvision.datasets.MNIST(root= data_path, train=True, download=True, transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+test_set = torchvision.datasets.MNIST(root= data_path, train=False, download=True,  transform=transforms.ToTensor())
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0)
+
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+acc_record = list([])
+loss_train_record = list([])
+loss_test_record = list([])
+
+
+net = SNN()
+net.to(device)
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    running_loss = 0
+    start_time = time.time()
+    for i, (images, labels) in enumerate(train_loader):
+        net.zero_grad()
+        optimizer.zero_grad()
+
+        images = images.float().to(device)
+        
+        images = images.view(100,-1)
+
+        outputs = net(images)
+        labels_ = torch.zeros(batch_size, 10).scatter_(1, labels.view(-1, 1), 1)
+        loss = criterion(outputs.cpu(), labels_)
+        running_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+        if (i+1)%100 == 0:
+             print ('Epoch [%d/%d], Step [%d/%d], Loss: %.5f'
+                    %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size,running_loss ))
+             running_loss = 0
+             print('Time elasped:', time.time()-start_time)
+    correct = 0
+    total = 0
+    optimizer = lr_scheduler(optimizer, epoch, learning_rate, 40)
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs = inputs.to(device)
+            inputs = inputs.view(100,-1)
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            labels_ = torch.zeros(batch_size, 10).scatter_(1, targets.view(-1, 1), 1)
+            loss = criterion(outputs.cpu(), labels_)
+            _, predicted = outputs.cpu().max(1)
+            total += float(targets.size(0))
+            correct += float(predicted.eq(targets).sum().item())
+            if batch_idx %100 ==0:
+                acc = 100. * float(correct) / float(total)
+                print(batch_idx, len(test_loader),' Acc: %.5f' % acc)
+
+    print('Iters:', epoch,'\n\n\n')
+    print('Test Accuracy of the model on the 10000 test images: %.3f' % (100 * correct / total))
+    acc = 100. * float(correct) / float(total)
+    acc_record.append(acc)
+    if epoch % 5 == 0:
+        print(acc)
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+            'acc_record': acc_record,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt' + names + '.t7')
+        best_acc = acc
---
+++ snn.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import sys
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+thresh = 0.5 # neuronal threshold
+lens = 0.5 # hyper-parameters of approximate function
+decay = 0.2 # decay constants
+num_classes = 10
+batch_size  = 100
+learning_rate = 1e-3
+num_epochs = 100 # max epoch
+
+
+
+
+
+class ActFun(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        return input.gt(thresh).float()
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        temp = abs(input - thresh) < lens
+        return grad_input * temp.float()
+
+act_fun = ActFun.apply
+
+# def mem_update(ops, x, mem, spike):
+#     mem = mem * decay * (1. - spike) + ops(x) 
+#     spike = act_fun(mem) # act_fun : approximation firing function
+#     return mem, spike
+
+def mem_update(ops, x, mem, spike, time_step):
+    # odd and even indices multiplex
+    # y = ops(x)
+
+    # mask_odd = torch.zeros(y.shape,device=device)
+    # mask_even = torch.zeros(y.shape,device=device)
+    # mask_odd[:,1::2] = 1
+    # mask_even[:,::2] = 1
+    # mask_odd = mask_odd.bool()
+    # mask_even = mask_even.bool()
+
+    # y_even = torch.masked_select(y,mask_even).view(batch_size,-1)
+    # y_odd = torch.masked_select(y,mask_odd).view(batch_size,-1)
+    is_odd = time_step % 2
+    y = ops(x)
+    # print(mem.shape)
+    # print(spike.shape)
+    # print(y.shape)
+    mem = mem * decay * (1. - spike[:,is_odd :: 2].clone()) + y[:, is_odd :: 2].clone()
+    spike = act_fun(mem) # act_fun : approximation firing function
+    return mem, spike
+
+    
+
+    
+
+# cnn_layer(in_planes, out_planes, stride, padding, kernel_size)
+cfg_cnn = [(1, 32, 1, 1, 3),
+           (32, 32, 1, 1, 3),]
+# kernel size
+cfg_kernel = [28, 14, 7]
+# fc layer
+cfg_fc = [128, 10]
+
+def lr_scheduler(optimizer, epoch, init_lr=0.1, lr_decay_epoch=50):
+    """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
+    if epoch % lr_decay_epoch == 0 and epoch > 1:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = param_group['lr'] * 0.1
+    return optimizer
+
+
+class SNN(nn.Module):
+    def __init__(self):
+        super(SNN, self).__init__()
+        self.input_layer = 784
+        self.hidden_layer = 60
+        self.output_layer = 10
+        self.muliplex_hidden_layer = 30
+        self.muliplex_output_layer = 5
+
+        self.fc1 = nn.Linear(self.input_layer,self.hidden_layer)
+        self.fc2 = nn.Linear(self.hidden_layer,self.output_layer)
+
+    def forward(self, input, time_window = 20):
+
+        h_spike = torch.zeros(batch_size,self.hidden_layer,device=device)
+        o_spike = o_sum = torch.zeros(batch_size,self.output_layer,device=device)
+
+        h_mem = torch.zeros(batch_size,self.muliplex_hidden_layer,device=device)
+        o_mem = torch.zeros(batch_size,self.muliplex_output_layer,device=device)
+
+        for step in range(time_window): # simulation time steps
+            x = input > torch.rand(input.size(), device=device) # prob. firing
+            if step % 2 == 0:   # even indices
+                h_mem, h_spike_even = mem_update(self.fc1, x.float(), h_mem, h_spike, step)
+                # h_sum += h_spike_even
+                h_spike[:,::2] = h_spike_even.clone()
+
+                o_mem, o_spike_even = mem_update(self.fc2, h_spike, o_mem, o_spike, step)
+                o_sum[:,::2] += o_spike_even.clone()
+                o_spike[:,::2] = o_spike_even.clone()
+            else:               # odd indices
+                h_mem, h_spike_odd = mem_update(self.fc1, x.float(), h_mem, h_spike, step)
+                # h_sum += h_spike_odd
+                h_spike[:,1::2] = h_spike_odd.clone()
+
+                o_mem, o_spike_odd = mem_update(self.fc2, h_spike, o_mem, o_spike, step)
+                o_sum[:,1::2] += o_spike_odd.clone()
+                o_spike[:,1::2] = o_spike_odd.clone()
+
+        outputs = o_sum / time_window
+        return outputs
